eebb8e1cd48b239aec57f140f27294afe318c4f3
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2019-02-25 10:21:07 +0100

    Set up router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
 
 import "./App.css";
+import HomePage from "./components/HomePage";
+import NotFound from "./components/NotFound";
 
 class App extends Component {
   render() {
@@ -8,7 +11,22 @@ class App extends Component {
       <div className="App">
         <header>
           <h1>Ironphones</h1>
+
+          <nav>
+            {/* NavLink to the home page should ALWAYS have exact on it */}
+            <NavLink exact to="/">
+              Home
+            </NavLink>
+          </nav>
         </header>
+
+        <Switch>
+          {/* home page route should ALWAYS have exact on it */}
+          <Route exact path="/" component={HomePage} />
+
+          {/* 404 route should go LAST */}
+          <Route component={NotFound} />
+        </Switch>
 
         <footer>
           <p>Made with 📱 at Ironhack</p>
---
+++ src/components/HomePage.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+
+import "./HomePage.css";
+
+class HomePage extends Component {
+  render() {
+    return (
+      <section className="HomePage">
+        <h2>Home Page</h2>
+        <p>Welcome to Ironphones! Your favorite store for overpriced phones.</p>
+      </section>
+    );
+  }
+}
+
+export default HomePage;
